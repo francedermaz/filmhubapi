13e7d9f4af4585af7f0d6d5112c643552a1547a5
francedermaz <francedermaz@gmail.com>
2024-10-05 20:29:04 -0300

    chore: cors issues

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 import router from './routes/routes.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+}));
+
 app.use(express.json());
 app.use(router);
 
