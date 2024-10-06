6caf0ce06b7f0cd1a39ec79a4ab61cc28b34b5ec
francedermaz <francedermaz@gmail.com>
2024-10-06 00:02:34 -0300

    feat: add endpoint to get one film

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,6 +4,7 @@ import {
   saveFavorite,
   getFavorites,
   getHealtz,
+  getMovieById,
 } from "../controllers/movieController.js";
 
 const router = express.Router();
@@ -12,5 +13,6 @@ router.get("/api/healtz", getHealtz);
 router.get("/api/movies", getMovies);
 router.post("/api/favorites", saveFavorite);
 router.get("/api/favorites", getFavorites);
+router.get("/api/movies/:id", getMovieById);
 
 export default router;
